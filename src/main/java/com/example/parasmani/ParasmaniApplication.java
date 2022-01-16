40875b02e9dbb1d28433258375a99be6870dfafc
parasmani <amritgaire18@gmail.com>
2022-01-16 15:41:41 +0100

    intial commit

---
+++ src/main/java/com/example/parasmani/ParasmaniApplication.java
@@ -0,0 +1,13 @@
+package com.example.parasmani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParasmaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParasmaniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parasmani/ParasmaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parasmani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParasmaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
